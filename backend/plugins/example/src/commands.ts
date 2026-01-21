772c85e92af1f079775c43695f7faa7bdec19e96
Lazy <2211717435@qq.com>
2026-01-22 02:59:00 +0800

    feat: 剩余参数

--- backend/plugins/example/src/commands.ts
+++ backend/plugins/example/src/commands.ts
@@ -4,12 +4,16 @@ import { initConfig, sendSingleMsg, utils as u } from "./utils";
 const UwU = "/test";
 export const cmd: CmdTree = {
   [UwU]: {
+    // _ 不支持剩余参数。
     _: (sender, sendTo, sApi) => {
       sendSingleMsg(sendTo, sApi, `执行 /test 的逻辑`);
     },
     qwq: {
       _: (sender, sendTo, sApi) => sendSingleMsg(sendTo, sApi, "执行 /test qwq 的逻辑"),
-      awa: (sender, sendTo, sApi) => sendSingleMsg(sendTo, sApi, "执行 /test qwq awa 的逻辑"),
+      awa: (sender, sendTo, sApi, ...args) => {
+        // console.log("执行 /test awa 的逻辑，参数：", args);
+        sendSingleMsg(sendTo, sApi, "执行 /test qwq awa 的逻辑");
+      },
     },
     reload: {
       _: (sender, sendTo, sApi) => {
@@ -25,7 +29,13 @@ export const handleCommand = (sender: number, sendTo: number, sApi: string, inpu
 
   let current: CmdTree | CmdHandler | CmdFn = cmd;
 
+  const args: string[] = [];
   for (const part of input) {
+    if (typeof current === "function") {
+      args.push(part);
+      continue;
+    }
+
     if (current[part]) {
       current = current[part];
     } else {
@@ -35,8 +45,8 @@ export const handleCommand = (sender: number, sendTo: number, sApi: string, inpu
   }
 
   if (typeof current === "function") {
-    (current as CmdFn)(sender, sendTo, sApi); // 执行命令
+    (current as any)(sender, sendTo, sApi, ...args); // 执行命令
   } else if (current._) {
-    (current as CmdHandler)._(sender, sendTo, sApi); // 执行命令
+    (current as any)._(sender, sendTo, sApi, ...args); // 执行命令
   }
 };
